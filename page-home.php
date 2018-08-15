eb3ec2d9e9f2264e60f9246cb68af0781aa0ed6d
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-15 13:52:53 -0500

    added jumbo

--- page-home.php
+++ page-home.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <div id="page-home">
     <div id="home-jumbo" class="container-fluid">
-       <?php get_template_part('template-parts/jumbos/', 'home'); ?>
+       <?php get_template_part('template-parts/jumbos/jumbo', 'home'); ?>
     </div>
 </div>
 
