487e4abd86ab7a8bd21ba0a99ce486ac68f90e1e
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-15 13:58:04 -0500

    updated navwalker

--- template-parts/navs/nav-top.php
+++ template-parts/navs/nav-top.php
@@ -1,6 +1,6 @@
   <nav class="navbar navbar-expand-md navbar-light bg-faded">
    <a class="navbar-brand" href="#">Navbar</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
    </button>
          <?php
@@ -8,7 +8,7 @@
      'menu'            => 'top',
      'theme_location'  => 'top',
      'container'       => 'div',
-     'container_id'    => 'bs4navbar',
+     'container_id'    => 'main-nav',
      'container_class' => 'collapse navbar-collapse',
      'menu_id'         => false,
      'menu_class'      => 'navbar-nav mr-auto',
