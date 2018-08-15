e12b253df26d98b7d482f9b2211c21fb3a8fb094
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-15 14:17:43 -0500

    modified nav-bar

--- template-parts/navs/nav-top.php
+++ template-parts/navs/nav-top.php
@@ -1,4 +1,4 @@
-  <nav class="navbar navbar-expand-md navbar-light bg-faded">
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
    <a class="navbar-brand" href="#">Navbar</a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
