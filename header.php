bbf434a99523897483815102ca335e9b7913db9e
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 17:12:35 -0500

    argh header styles enqued

--- header.php
+++ header.php
@@ -11,8 +11,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Custom styles for this template -->
-    <!--<link href="product.css" rel="stylesheet">-->
-    <!-- <link href="css/autumn2018.css" rel="stylesheet"> -->
     <?php wp_head(); ?>
   </head>
   <body>
