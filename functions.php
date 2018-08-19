73f2faa1bceead7cae504d7b46f9a2e83f05d6c7
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 17:15:41 -0500

    changed function to autumn2018_styles

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php 
 //enque styles
 
-function colab_theme_styles() {
+function autumn2018_styles() {
     wp_register_style('autumn2018', get_template_directory_uri() . '/css/autumn2018.css', __FILE__);
     wp_enqueue_style('autum2018');
 }
 
 
-add_action( 'wp_enqueue_scripts', 'colab_theme_styles' );  
+add_action( 'wp_enqueue_scripts', 'autum2018_styles' );  
 
 // Include custom navwalker
 require_once('bs4navwalker.php');
