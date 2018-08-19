574d67317c3949a5d14aac7a74b3a54eb406ad82
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 17:42:01 -0500

    template parts

--- page-home.php
+++ page-home.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
 <div id="page-home">
-    <div id="home-jumbo" class="container-fluid">
+    <div id="home-header" class="container-fluid">
        <?php get_template_part('template-parts/sections/section', 'header'); ?>
     </div>
 </div>
