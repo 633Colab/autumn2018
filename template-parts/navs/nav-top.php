7a15054105b8a94050d17bf5bb23f76b467762ff
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 18:11:30 -0500

    navbar changes

--- template-parts/navs/nav-top.php
+++ template-parts/navs/nav-top.php
@@ -1,6 +1,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div>
    <a class="navbar-brand" href="#">Navbar</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
    </button>
          <?php
@@ -10,11 +11,12 @@
      'container'       => 'div',
      'container_id'    => 'navbarResponsive',
      'container_class' => 'collapse navbar-collapse',
-     'menu_id'         => 'navbarID',
-     'menu_class'      => 'navbar-nav mr-auto',
+     'menu_id'         => false,
+     'menu_class'      => 'navbar-nav text-uppercase ml-auto',
      'depth'           => 2,
      'fallback_cb'     => 'bs4navwalker::fallback',
      'walker'          => new bs4navwalker()
    ]);
    ?>
+   </div>
     </nav>
