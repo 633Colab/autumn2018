8dc74a1f087f8f4156fb471e04ed701c83d6a76f
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 17:06:13 -0500

    added enque styles

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php 
+//enque styles
+
+function colab_theme_styles() {
+    wp_register_style('autumn2018', get_template_directory_uri() . '/css/autumn2018.css', __FILE__));
+    wp_enqueue_style('autum2018');
+}
+
+add_action( 'wp_enqueue_scripts', 'colab_theme_styles' );  
 // Include custom navwalker
 require_once('bs4navwalker.php');
 
 // Register WordPress nav menu
 register_nav_menu('top', 'Top menu');
+
--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <!--<link href="product.css" rel="stylesheet">-->
-    <link href="css/autumn2018.css" rel="stylesheet">
+    <!-- <link href="css/autumn2018.css" rel="stylesheet"> -->
   </head>
   <body>
   <?php get_template_part('template-parts/navs/nav', 'top'); ?>
