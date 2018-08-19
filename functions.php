5fb8fba84996d25dd0b1f61b1dd09a04c53606b2
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 17:18:32 -0500

    corrected autum to autumn

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 
 function autumn2018_styles() {
     wp_register_style('autumn2018_css', get_template_directory_uri() . '/css/autumn2018.css', __FILE__);
-    wp_enqueue_style('autum2018_css');
+    wp_enqueue_style('autumn2018_css');
 }
 
 
-add_action( 'wp_enqueue_scripts', 'autum2018_styles' );  
+add_action( 'wp_enqueue_scripts', 'autumn2018_styles' );  
 
 // Include custom navwalker
 require_once('bs4navwalker.php');
