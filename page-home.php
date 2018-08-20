d1451e94b868aba5e088d09bdbbec401344d427e
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 19:05:35 -0500

    added services/icon section

--- page-home.php
+++ page-home.php
@@ -3,6 +3,7 @@
     <div id="home-header">
        <?php get_template_part('template-parts/sections/section', 'header'); ?>
     </div>
+    <?php get_template_part('template-parts/sections/section','icon'); ?>
 </div>
 
 <?php get_footer(); ?>
