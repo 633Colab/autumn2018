4aa03a81cc8d52710c53eece02cbaadc3961a619
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 19:13:37 -0500

    added portfolio

--- page-home.php
+++ page-home.php
@@ -4,6 +4,8 @@
        <?php get_template_part('template-parts/sections/section', 'header'); ?>
     </div>
     <?php get_template_part('template-parts/sections/section','icon'); ?>
+
+    <?php get_template_part('template-parts/sections/section','portfolio'); ?>
 </div>
 
 <?php get_footer(); ?>
