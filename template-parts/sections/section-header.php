8361a45328b1ed953bd854e14889ae846d298a13
Jimmy Doyle <jkdoyle@gmail.com>
2018-08-19 22:40:46 -0500

    masthead background image

--- template-parts/sections/section-header.php
+++ template-parts/sections/section-header.php
@@ -1,6 +1,6 @@
 
     <!-- Header -->
-    <header class="masthead" style="background-image: url('images/header-bg.jpg');">
+    <header class="masthead" style="background-image: url('/wp-content/themes/autumn2018/images/header-bg.jpg');">
       <div class="container">
         <div class="intro-text">
           <div class="intro-lead-in">Welcome To Our Studio!</div>
